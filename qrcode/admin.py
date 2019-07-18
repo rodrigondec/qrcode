44d1bceedc482a662d65959460c50a7e32ef6b44
rodrigondec <rodrigondec@gmail.com>
2019-07-18 16:59:05 -0300

    qrcode admin

---
+++ qrcode/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+
+from qrcode.models import QrCode, UrlQrCode, FileQrCode
+
+
+@admin.register(UrlQrCode)
+class ModelBAdmin(PolymorphicChildModelAdmin):
+    base_model = UrlQrCode
+
+
+@admin.register(FileQrCode)
+class ModelCAdmin(PolymorphicChildModelAdmin):
+    base_model = FileQrCode
+
+
+@admin.register(QrCode)
+class ModelAParentAdmin(PolymorphicParentModelAdmin):
+    base_model = QrCode
+    child_models = (UrlQrCode, FileQrCode)
+    list_filter = (PolymorphicChildModelFilter,)
