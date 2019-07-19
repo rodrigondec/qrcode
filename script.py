54b97a1afd98b4f14551bd65c75952268f5c62ee
rodrigondec <rodrigondec@gmail.com>
2019-07-18 21:03:18 -0300

    renamed script

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ from qr.models import QrCode, FileQrCode, UrlQrCode
 
 fqr = QrCode.objects.instance_of(FileQrCode).first()
 print(fqr.file.name, fqr.file.url)
-fqr.build_img()
+fqr.build_image()
 
 furl = QrCode.objects.instance_of(UrlQrCode).first()
 print(furl.url)
-furl.build_img()
+furl.build_image()
