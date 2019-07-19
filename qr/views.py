b2a3f2ab48bd5658559c630482e410ed3509e061
rodrigondec <rodrigondec@gmail.com>
2019-07-18 21:02:55 -0300

    Resolve url view

--- qr/views.py
+++ qr/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views import View
+from django.shortcuts import redirect
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+from qr.models import QrCode
+
+
+class GetQrCodeView(View):
+    @staticmethod
+    def get(request, pk):
+        try:
+            qr_code = QrCode.objects.get(id=pk)
+        except (QrCode.DoesNotExist, ValidationError):
+            raise Http404("Qr Code não existe")
+        return redirect(qr_code.resolve())
