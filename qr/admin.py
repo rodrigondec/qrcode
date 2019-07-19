ea1d72ff68e745fbf40c1ed505ecc533971edba2
rodrigondec <rodrigondec@gmail.com>
2019-07-18 21:03:11 -0300

    qr admin with better fields

--- qr/admin.py
+++ qr/admin.py
@@ -3,15 +3,23 @@ from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModel
 
 from qr.models import QrCode, UrlQrCode, FileQrCode
 
+BASE_DISPLAY_LIST = ['id', 'name', 'image']
+
 
 @admin.register(UrlQrCode)
 class ModelBAdmin(PolymorphicChildModelAdmin):
     base_model = UrlQrCode
+    show_in_index = True
+    list_display = BASE_DISPLAY_LIST + ['url']
+    exclude = ['image']
 
 
 @admin.register(FileQrCode)
 class ModelCAdmin(PolymorphicChildModelAdmin):
     base_model = FileQrCode
+    show_in_index = True
+    list_display = BASE_DISPLAY_LIST + ['file']
+    exclude = ['image']
 
 
 @admin.register(QrCode)
@@ -19,3 +27,5 @@ class ModelAParentAdmin(PolymorphicParentModelAdmin):
     base_model = QrCode
     child_models = (UrlQrCode, FileQrCode)
     list_filter = (PolymorphicChildModelFilter,)
+    list_display = BASE_DISPLAY_LIST
+    exclude = ['image']
