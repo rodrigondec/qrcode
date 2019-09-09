d5d1849e553a32317f8bdde6148e007348d07688
rodrigondec <rodrigondec@gmail.com>
2019-09-09 16:42:40 -0300

    script for insert logo on qr

--- script.py
+++ script.py
@@ -1,16 +1,7 @@
-from qr.models import QrCode, FileQrCode, UrlQrCode
+from qr.models import QrCode
+from logos.models import Logo
 
-fqr = QrCode.objects.instance_of(FileQrCode).first()
-print(fqr.file.name, fqr.file.url)
-fqr.build_image()
+code = QrCode.objects.first()
+logo = Logo.objects.first()
 
-furl = QrCode.objects.instance_of(UrlQrCode).first()
-print(furl.url)
-furl.build_image()
-
-
-from qr.utils import _create_qrcode_img, insert_logo_on_img
-
-img = _create_qrcode_img('Some Data')
-img = insert_logo_on_img(img)
-img.save('t.png')
+code.build_image(logo)
