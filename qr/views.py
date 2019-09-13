297af550266f772e6f620d28e3c146aab05c52df
rodrigondec <rodrigondec@gmail.com>
2019-09-12 21:09:08 -0300

    CreateView with 2 forms on context

--- qr/views.py
+++ qr/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
 from qr.models import QrCode, FileQrCode, UrlQrCode
-from qr.forms import QrCodeForm, UrlQrCodeForm, FileQrCodeForm
+from qr.forms import UrlQrCodeForm, FileQrCodeForm
 
 
 class ResolveQrCodeView(View):
@@ -23,25 +23,34 @@ class QrCodeListView(ListView):
     model = QrCode
     template_name = 'qr/list.html'
     context_object_name = "qrcodes"
-    paginate_by = 10
+    paginate_by = 5
 
 
 class QrCodeCreateView(View):
-    form_class = QrCodeForm
-    template_name = 'qr/select_create.html'
+    template_name = 'qr/create.html'
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+        context = {
+            'file_form': FileQrCodeForm(),
+            'url_form': UrlQrCodeForm()
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            print(form)
-            return render(request, self.template_name, {'form': form})
-
-        return render(request, self.template_name, {'form': form})
-
+        context = {
+            'file_form': FileQrCodeForm(),
+            'url_form': UrlQrCodeForm()
+        }
+        if request.POST.get('type'):
+            if request.POST.get('type') == 'url':
+                form = UrlQrCodeForm(request.POST)
+                context['url_form'] = form
+            else:
+                form = FileQrCodeForm(request.POST, request.FILES)
+                context['file_form'] = form
+            if form.is_valid():
                 form.save()
                 return render(request, self.template_name, context)
             context['form_errors'] = form.errors
+            return render(request, self.template_name, context)
+        raise Exception('Não teve um tipo de formulário retornado!')
