b127735c0336626d5c0d9d4b37f9d621f99042a1
rodrigondec <rodrigondec@gmail.com>
2019-09-12 21:05:38 -0300

    only 2 forms

--- qr/forms.py
+++ qr/forms.py
@@ -3,17 +3,13 @@ from django.forms import ModelForm, Form, ChoiceField
 from qr.models import QrCode, UrlQrCode, FileQrCode
 
 
-class QrCodeForm(Form):
-    qr_class = ChoiceField(choices=((1, 'file'), (2, 'url')), label="", initial='', required=True)
-
-
 class UrlQrCodeForm(ModelForm):
     class Meta:
         model = UrlQrCode
-        exclude = ['img']
+        fields = ['name', 'url']
 
 
 class FileQrCodeForm(ModelForm):
     class Meta:
         model = FileQrCode
-        exclude = ['img']
+        fields = ['name', 'file']
