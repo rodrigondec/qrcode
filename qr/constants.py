db6e85aad91d5dcf17d35a16353234332f2d0832
rodrigondec <rodrigondec@gmail.com>
2019-10-15 12:50:56 -0300

    constants parametrized

--- qr/constants.py
+++ qr/constants.py
@@ -1,10 +1,12 @@
 from qrcode.constants import ERROR_CORRECT_L
 
+from decouple import config
 
-LOGO_SIZE = 120
-VERSION = 1
-ERROR_CORRECTION_LEVEL = ERROR_CORRECT_L
-BOX_SIZE = 10
-BORDER = 2
+
+LOGO_SIZE = config('QR_LOGO_SIZE', default=120, cast=int)
+VERSION = config('QR_VERSION', default=3, cast=int)
+ERROR_CORRECTION_LEVEL = config('QR_CORRECTION', default=2, cast=int)
+BOX_SIZE = config('QR_BOX_SIZE', default=10, cast=int)
+BORDER = config('QR_BORDER', default=2, cast=int)
 
 LABEL_SIZE = 6
