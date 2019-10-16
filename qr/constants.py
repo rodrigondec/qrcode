a3ee63e976c88a0626ef7a6b7698ff855a75dd9e
rodrigondec <rodrigondec@gmail.com>
2019-10-16 19:01:12 -0300

    leaflet constants

--- qr/constants.py
+++ qr/constants.py
@@ -10,3 +10,17 @@ QR_BOX_SIZE = config('QR_BOX_SIZE', default=10, cast=int)
 QR_BORDER = config('QR_BORDER', default=4, cast=int)
 
 QR_LABEL_SIZE = 6
+
+LEAFLET_ICON = config('LEAFLET_ICON', default="map-marker-alt")
+LEAFLET_SHAPE = config('LEAFLET_SHAPE', default="marker")
+LEAFLET_BACKGROUND_COLOR = config('LEAFLET_BACKGROUD_COLOR', default="white")
+
+LEAFLET_COLOR_STEP = config('LEAFLET_COLOR_STEP', default=5, cast=int)
+LEAFLET_COLORS = [
+    config('LEAFLET_COLOR_1', default='#cc0808'),
+    config('LEAFLET_COLOR_2', default='#ccc908'),
+    config('LEAFLET_COLOR_3', default='#0fcc08'),
+    config('LEAFLET_COLOR_4', default='#0846cc'),
+    config('LEAFLET_COLOR_5', default='#b808cc')
+]
+
