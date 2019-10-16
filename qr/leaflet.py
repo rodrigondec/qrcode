d2f04364b4da69b38a3e57f539bd93ca095a8098
rodrigondec <rodrigondec@gmail.com>
2019-10-16 19:06:02 -0300

    LeafletColorManager created

--- qr/leaflet.py
+++ qr/leaflet.py
@@ -17,3 +17,24 @@ class LeafletInterval:
         if value in self.interval:
             return self.color
         return None
+
+
+class LeafletColorManager:
+    intervals = [
+        LeafletInterval(
+            index*LEAFLET_COLOR_STEP,
+            (index*LEAFLET_COLOR_STEP)+LEAFLET_COLOR_STEP if index <= len(LEAFLET_COLORS)-2 else inf,
+            color
+        )
+        for index, color in enumerate(LEAFLET_COLORS)
+    ]
+
+    @classmethod
+    def pick(cls, value: int):
+        if value < 0:
+            raise ValueError('Não é possível procurar um valor menor do que 0!')
+        for interval in cls.intervals:
+            color = interval.pick(value)
+            if color is not None:
+                return color
+        raise Exception('Não foi encontrado um valor para o intervalo!')
