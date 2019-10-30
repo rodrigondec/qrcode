8c451edb87af029f86911d496915aa38dc7b2c41
rodrigondec <rodrigondec@gmail.com>
2019-10-30 14:47:18 -0300

    LoginRequiredMixin

--- logos/views.py
+++ logos/views.py
@@ -1,17 +1,18 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from logos.models import Logo
 
 
-class LogoListView(ListView):
+class LogoListView(LoginRequiredMixin, ListView):
     model = Logo
     template_name = 'logos/list.html'
     context_object_name = "logos"
     paginate_by = 5
 
 
-class LogoCreateView(CreateView):
+class LogoCreateView(LoginRequiredMixin, CreateView):
     model = Logo
     template_name = 'generic/create_update.html'
     fields = ['name', 'image']
@@ -21,7 +22,7 @@ class LogoCreateView(CreateView):
     }
 
 
-class LogoUpdateView(UpdateView):
+class LogoUpdateView(LoginRequiredMixin, UpdateView):
     model = Logo
     template_name = 'generic/create_update.html'
     fields = ['name', 'image']
