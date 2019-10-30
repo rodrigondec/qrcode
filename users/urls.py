db5c303091d408ee80162da9ba7dda04ebc7ef58
rodrigondec <rodrigondec@gmail.com>
2019-10-30 14:48:21 -0300

    user urls

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from users.views import MyPasswordChangeView, MyPasswordResetView
+
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_change/', MyPasswordChangeView.as_view(), name='password_change'),
+
+    path('password_reset/', MyPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
