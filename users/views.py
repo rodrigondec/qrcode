0205f22d4cc8cb062fc705141c8e79b61f0e3d7c
rodrigondec <rodrigondec@gmail.com>
2019-10-30 15:04:54 -0300

    MyPasswordResetConfirmView

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth.views import PasswordChangeView, PasswordResetView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 
 
 class MyPasswordChangeView(PasswordChangeView):
@@ -28,4 +28,18 @@ class MyPasswordResetView(PasswordResetView):
             'o e-mail que você forneceu. Você receberá a mensagem em breve.\n '
             'Se você não receber um e-mail, por favor verifique se você digitou o endereço que você usou para '
             'se registrar, e verificar a sua pasta de spam.')
+        return super().form_valid(form)
+
+
+class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'generic/create_update_without_nav.html'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(titulo='Resetar senha', **kwargs)
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            'Sua senha foi definida. Você pode prosseguir e se autenticar agora.')
         return super().form_valid(form)
