5b5edaebaab4c6df455829234d5b5bc9ceadce51
rodrigondec <rodrigondec@gmail.com>
2019-10-30 15:04:38 -0300

    renamed links

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from users.views import MyPasswordChangeView, MyPasswordResetView
+from users.views import MyPasswordChangeView, MyPasswordResetView, MyPasswordResetConfirmView
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('password_change/', MyPasswordChangeView.as_view(), name='password_change'),
+    path('mudar_senha/', MyPasswordChangeView.as_view(), name='password_change'),
 
-    path('password_reset/', MyPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('resetar_senha/', MyPasswordResetView.as_view(), name='password_reset'),
+    path('resetar/<uidb64>/<token>/', MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
