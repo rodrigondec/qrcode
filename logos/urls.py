74aef1947b6539e2494d18ab422520aa184f7092
rodrigondec <rodrigondec@gmail.com>
2019-10-30 14:47:06 -0300

    include urls

--- logos/urls.py
+++ logos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from logos.views import LogoListView, LogoCreateView, LogoUpdateView
 
 urlpatterns = [
-    path('logos/listar/', LogoListView.as_view()),
-    path('logos/cadastrar/', LogoCreateView.as_view()),
-    path('logos/atualizar/<pk>/', LogoUpdateView.as_view())
+    path('listar/', LogoListView.as_view()),
+    path('cadastrar/', LogoCreateView.as_view()),
+    path('atualizar/<pk>/', LogoUpdateView.as_view())
 ]
